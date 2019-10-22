3fcf5f8b960b1fe82a889633aaa36a3fbd3d0dc3
Christina Hoang <christinahoang@CHOANG-2.local>
2019-10-21 20:38:26 -0500

    add Enemy class

---
+++ group4_assignment6/Enemy.swift
@@ -0,0 +1,24 @@
+//
+//  Enemy.swift
+//  group4_assignment6
+//
+//  Created by Christina Hoang on 10/21/19.
+//  Copyright © 2019 cs329e. All rights reserved.
+//
+
+import UIKit
+
+class Enemy {
+    var name: String
+    var level: Int
+    var attackModifiers: Int
+    var hitPoints: Int
+    
+    init (name:String, level: Int, attackModifiers: Int, hitPoints: Int) {
+        self.name = name
+        self.level = level
+        self.attackModifiers = attackModifiers
+        self.hitPoints = hitPoints
+    }
+    
+}
